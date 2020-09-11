f1c83eb3e28e2f1876ec463807ded3ce87ee8195
Graeme Meyer <graememeyer@hotmail.com>
2020-09-11 18:27:56 +0100

    initial commit

---
+++ debug.ps1
@@ -0,0 +1,40 @@
+# debug.ps1
+Clear-Host
+#Import-Module .\Power-Cbr.psd1
+Import-Module .\Tools\Invoke-CbrApi.psm1 -Force
+foreach ($Module in (Get-ChildItem .\*.psm1 -Recurse | Select-Object -ExpandProperty FullName)) {Import-Module $Module -Force}
+
+# Ignore the self-signed certificate.
+if (-not("dummy" -as [type])) {
+    add-type -TypeDefinition @"
+using System;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+public static class Dummy {
+    public static bool ReturnTrue(object sender,
+        X509Certificate certificate,
+        X509Chain chain,
+        SslPolicyErrors sslPolicyErrors) { return true; }
+
+    public static RemoteCertificateValidationCallback GetDelegate() {
+        return new RemoteCertificateValidationCallback(Dummy.ReturnTrue);
+    }
+}
+"@
+}
+
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = [dummy]::GetDelegate()
+
+$Sensors = Invoke-CbrApi -Uri "/api/v1/sensor" -Method "GET" | Select-Object -First 100
+""
+"Retrieved $($Sensors.count) sensors."
+
+$Alerts = Invoke-CbrApi -Uri "/api/v2/alert" -Method "GET" | Select-Object -First 100 | Select-Object -ExpandProperty "Results"
+""
+"Retrieved $($Alerts.count) alerts."
+$Alerts
+
+$Alert = Get-CbrAlert -Id "de887c1f-074c-49c5-a1c0-0b7849e8a842"
+$Alert
