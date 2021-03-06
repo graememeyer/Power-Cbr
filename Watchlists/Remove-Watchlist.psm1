07d2173bde224e364f4f1b7cd05e797141d7e0a7
Graeme Meyer <graeme.meyer@dxc.com>
2021-03-06 10:10:20 +0000

    Remove-Watchlist pipeline

--- Watchlists/Remove-Watchlist.psm1
+++ Watchlists/Remove-Watchlist.psm1
@@ -2,21 +2,23 @@
 Function Remove-Watchlist {
     [alias("Delete-Watchlist")]
     param(
-        [Parameter(mandatory=$True)] [int]$Id,
+        [Parameter(mandatory=$True, ValueFromPipelineByPropertyName=$True)] [int]$Id,
         [Parameter(mandatory=$False)] [string]$Instance
     )
-    $UriPath = "/api/v1/watchlist"
-    $Method = "DELETE"
-
-    # Single alert query
-    if ($Id) {
-        $UriPath = $UriPath + "/$Id"
-    }
-
-    if ($Instance) {
-        Invoke-CbrApi -Uri $UriPath -Method $Method -Instance $Instance
-    }
-    else {
-        Invoke-CbrApi -UriPath $UriPath -Method $Method
+    process {
+        $UriPath = "/api/v1/watchlist"
+        $Method = "DELETE"
+    
+        # Single alert query
+        if ($Id) {
+            $UriPath = $UriPath + "/$Id"
+        }
+        
+        if ($Instance) {
+            Invoke-CbrApi -Uri $UriPath -Method $Method -Instance $Instance
+        }
+        else {
+            Invoke-CbrApi -UriPath $UriPath -Method $Method
+        }
     }
 }
